2aaa70decc3bac4882d4ef1bef7a615006faf8e8
catbelowakeyboard <rhonnapeevor@gmail.com>
2025-10-05 21:09:12 +0700

    cookiepremium.lua

---
+++ cookiepremium.lua
@@ -0,0 +1,104 @@
+userautobuy = ""
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Crystal | Cookie Clicker: Premium",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "CCS",
+   LoadingSubtitle = "By Crystal Development Team",
+   ShowText = "ilovecrystal", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "AmberGlow", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Paid Cookie Clicker Key-System",
+      Subtitle = "By CDT",
+      Note = "Enter your premium key, or buy one.", -- Use this to tell the user how to get a key
+      FileName = "CDTkeysys", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"CDT.irt"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+Rayfield:Notify({
+   Title = "Thanks for using premium!",
+   Content = "We appriciate you buying premium! (buying via dming)",
+   Duration = 6.5,
+   Image = "crown",
+})
+
+
+local auto = Window:CreateTab("Automatic", "table-cells-split") -- Title, Image
+local settings = Window:CreateTab("Settings", "settings") -- Title, Image
+
+local ac = auto:CreateSection("Clicking")
+local autoclicktogg = auto:CreateToggle({
+   Name = "Auto Click",
+   CurrentValue = false,
+   Flag = "autoclick", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   while Value do
+        game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Cookie"):FireServer()
+        wait(0.05)
+    end
+   end,
+})
+local customautoclick = auto:CreateInput({
+   Name = "CUstom autoclick value",
+   CurrentValue = "",
+   PlaceholderText = "How much time you want to click",
+   RemoveTextAfterFocusLost = true,
+   Flag = "cac",
+   Callback = function(Text)
+   for i = 1, Text do
+          game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Cookie"):FireServer()
+    end
+   end,
+})
+local ab = auto:CreateSection("Auto buying")
+local autobuy = auto:CreateDropdown({
+   Name = "Buying:",
+   Options = {"Clicker","grandma"},
+   CurrentOption = {""},
+   MultipleOptions = false,
+   Flag = "autobuy dropdown", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Options)
+   userautobuy = Options[1]
+   print(userautobuy)
+   end,
+})
+local Toggle = auto:CreateToggle({
+   Name = "Start Buying",
+   CurrentValue = false,
+   Flag = "startbuy", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   if userautobuy == "Clicker" then
+       while Value do
+           local args = {
+	"Cursor",
+	1,
+	true
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyBuilding"):FireServer(unpack(args))
+wait(0.05)
+       end
+end
+   end,
+})
